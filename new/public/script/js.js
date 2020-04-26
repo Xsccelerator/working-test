ec988df0ab3f65670eddf9eb2bf963e199d114e5
Oleksii Kravets <kravets.oleksii.noone@gmail.com>
2020-04-26 12:02:35 +0200

    update

--- new/public/script/js.js
+++ new/public/script/js.js
@@ -8,17 +8,40 @@ const toDos= [
   'Еще какое-то задание'
 ]
   $(document).ready(function(){
+    //Подключаем скрипты materialize для работы табов
     $('.collapsible').collapsible();
     $('.tabs').tabs();
+    //Вызываем функции отображения в момент загрузки чтобы исправить баг
+    //(в момент загрузки табы были пустыми и заполнялись только когда начинал кликать)
     oldTabView()
     newTabView()
+    // jsonView()
   });
+//Получили JSON и преобразовали его в массив, который пушим старый массив toDos 
+// var jsonView = function(){
+  $.getJSON('todos.json', function(responseJSON){
+   responseJSON.map(function (item){
+     return item.description
+    //  toDos.push(item.description)
+   })
+  })
+// }
+
+
+
+
+
+
+
+
+
  //////////////////////////////////////////////////
+ //Скрываем первый экран, нужно доработать связав результат с Local storage
   $('.btn-center').on('click', function(){
     $('.content').slideUp(700) 
   })
  ////////////////////////////////////////// 
-  
+ //Вешаем на Enter событие отправки сообщения в массив toDos 
   var textArea = $('.text-area') 
   textArea.on('keypress', function(e){
     
@@ -32,13 +55,14 @@ const toDos= [
     }
   })
   
-
+//Функция котороая отображает элементы во вкладке Старые
   var oldTabView = function(e){  
     var ulOld = $('.oldTodos').empty()
       toDos.forEach(function(item){
       ulOld.append($(`<li>${item}</li>`))
   })
 } 
+//Функция котороая отображает элементы во вкладке Новые
 var newTabView = function(e){
   var ul = $('.newTodos').empty()
   toDos.forEach(function(item){
@@ -46,11 +70,12 @@ var newTabView = function(e){
   })
 }
 ///////////////////////////////////////////////
+//Получаем все табы
 var tab = $('.tab')
-
+//Детектим вкладку на которой совершен клик
  tab.on('click', function(e){
   let par = e.target.dataset.par
-  console.log(par)
+  
     if(par === "new" ){
       newTabView()    
     }
